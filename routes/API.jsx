bf97cad40eab7b9bd54d4892ee1db92a56be8e34
dagreatcode <dagreatcode@gmail.com>
2020-11-05 23:29:55 -0500

    added the minimum 2 pages

---
+++ routes/API.jsx
@@ -0,0 +1,10 @@
+// 6. Add the following Express routes for your app:
+
+// * `/api/books` (get) - Should return all saved books as JSON.
+
+// * `/api/books` (post) - Will be used to save a new book to the database.
+
+// * `/api/books/:id` (delete) - Will be used to delete a book from the database by Mongo `_id`.
+
+// * `*` (get) - Will load your single HTML page in `client/build/index.html`. 
+//         Make sure you have this _after_ all other routes are defined.
