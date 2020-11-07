0aa6c0b8b266c8ebd5eef587d4f645a192d6eea0
dagreatcode <dagreatcode@gmail.com>
2020-11-07 18:33:02 -0500

    need a home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./containers/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Saved from "../src/pages/savedController";
+import Search from "../src/pages/searchController";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+            <Navbar />
+            <Switch>
+              <Route exact path="/books/saved" component={SaveBook} />
+              <Route exact path="/books/search" component={SearchBook} />
+              <Route exact path="/books" component={AllBooks} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+      </Router>
     </div>
   );
 }
