4ec6c5d75b2c1f97ea59689011488b462fbf1a9c
dagreatcode <dagreatcode@gmail.com>
2020-11-07 23:12:28 -0500

    changed my routes folder to utils also added my nice looking CRUD routes to my booksController

--- controllers/booksController.jsx
+++ controllers/booksController.jsx
@@ -1,11 +1,45 @@
+const express = require("express");
+const router = express.Router();
 const db = require("../models");
 
-// Defining methods for the booksController
-module.exports = {
-  findAll: function(req, res) {
-    db.Book
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
+// TODO: Build an authentication/authorization controller.
+
+router.get("/", (req, res) => {
+    // TODO: Restrict which fields are returned. NO PASSWORD!
+  db.User.find({})
+    .populate("books")
+    .then((foundUsers) => {
+      res.json(foundUsers);
+    });
+});
+
+router.get("/:id", (req, res) => {
+    // TODO: Restrict which fields are returned. NO PASSWORD!
+  db.User.find({ _id: req.params.id }).then((foundUser) => {
+    res.json(foundUser);
+  });
+});
+
+router.post("/", (req, res) => {
+  db.User.create(req.body).then((newUser) => {
+    res.json(newUser);
+  });
+});
+
+router.put("/:id", (req, res) => {
+    // TODO: Restrict which fields are editable
+  db.User.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+    (updatedUser) => {
+      res.json(updatedUser);
+    }
+  );
+});
+
+router.delete("/:id", (req, res) => {
+    // TODO: Figure out how to restrict account deletion.
+  db.User.findByIdAndDelete(req.params.id).then((result) => {
+    res.json(result);
+  });
+});
+
+module.exports = router;
