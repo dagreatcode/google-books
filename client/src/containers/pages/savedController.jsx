b9aca8131d0003c68c2b72da60ceb15bd837dd1e
dagreatcode <dagreatcode@gmail.com>
2020-11-07 21:40:53 -0500

    adding the page tables to saved

--- client/src/containers/pages/savedController.jsx
+++ client/src/containers/pages/savedController.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const savedController = () => {
-    return (
-        <div>
-            <h1>Saved Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Saved Page</h1>
+      <table class="table">
+        <caption>List of users</caption>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+      Z¸¸
+    </div>
+  );
 };
 
 export default savedController;
